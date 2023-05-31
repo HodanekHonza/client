bf7b53cd5bece18e3b058ca4687a5ef5ea11039e
Jan Hodánek <98991609+HodanekHonza@users.noreply.github.com>
2023-05-31 13:45:35 +0200

    Update TopicsApi.js

--- src/api/TopicsApi.js
+++ src/api/TopicsApi.js
@@ -1,6 +1,6 @@
 const fetchTopics = async () => {
     try {
-      const response = await fetch("http://localhost:8000/topics/list"); // Replace with your API endpoint
+      const response = await fetch("http://localhost:8000/topics/list"); 
       const topics = await response.json();
       return topics;
     } catch (error) {
